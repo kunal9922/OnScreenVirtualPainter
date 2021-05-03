734e1de738b8a8db6f7b56fb82f5dcc650c5e80a
kunal <53283003+kunal9922@users.noreply.github.com>
2021-05-03 21:03:15 +0530

    Able to read header images for drawing tools picker

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ import numpy as np
 import time
 import os
 import cv2
+import handTrackingMini as htm
+
+folderPath = "HeadersAIPainter"
+myList = os.listdir(folderPath)
+print(myList)
+
+headerImgs = []
+for imgPath in myList:
+      image = cv2.imread(f'{folderPath}/{imgPath}')
+      headerImgs.append(image)
+
+print(len(headerImgs))
