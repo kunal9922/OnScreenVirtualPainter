7635f9be614ebdade9214f03abfd45708630a5a9
kunal <53283003+kunal9922@users.noreply.github.com>
2021-05-03 21:52:31 +0530

    stich the HeaderImage as a paintTools into canvas where we have to paint

--- main.py
+++ main.py
@@ -8,9 +8,26 @@ folderPath = "HeadersAIPainter"
 myList = os.listdir(folderPath)
 print(myList)
 
+# read the header images for paint tools selection
 headerImgs = []
 for imgPath in myList:
       image = cv2.imread(f'{folderPath}/{imgPath}')
       headerImgs.append(image)
 
 print(len(headerImgs))
+
+#deafault header image for nothing to selected
+header1 = headerImgs[2]
+header1 = cv2.resize(header1, (720, 130))
+
+#read from WebCam here
+cap = cv2.VideoCapture(r"dataSets//handTestVideo.mp4")
+while cap.isOpened():
+      success, imgFrame = cap.read()
+      imgFrame = cv2.resize(imgFrame, (720, 480))
+
+      #Setting the header image
+      imgFrame[0:130, 0:720] = header1
+      cv2.imshow("TestFrames", imgFrame)
+      
+      cv2.waitKey(1)
